bb33d1b105bb6c725720a20a08494667f50bd4c5
luigimatchps <luigi.c@matchps.com>
2023-05-31 13:23:04 -0300

    Adding assertion to navigation test cases

--- cypress/e2e/sanity.cy.js
+++ cypress/e2e/sanity.cy.js
@@ -2,49 +2,61 @@ import urls from '../fixtures/urls.json'
 describe("Navigate to Ernesta's Pages", () => { 
   it('Homepage', () => {
     cy.visit(urls.Base)
+    cy.title().should().not().be('Not found | Ernesta');
   })
 
   it('Product Listing Page', () => {
     cy.visit(urls.PLP)
+    cy.title().should().not().be('Not found | Ernesta');
   })
 
   it('About Us Page', () => {
     cy.visit(urls.AboutUs)
+    cy.title().should().not().be('Not found | Ernesta');
   })
 
   it('Beta Launch Page', () => {
     cy.visit(urls.BetaLaunch)
+    cy.title().should().not().be('Not found | Ernesta');
   })
 
   it('Care Guide Page', () => {
     cy.visit(urls.CareGuide)
+    cy.title().should().not().be('Not found | Ernesta');
   })
 
   it('Limited Warranty Page', () => {
     cy.visit(urls.LimitedWarranty)
+    cy.title().should().not().be('Not found | Ernesta');
   })
 
   it('Materials Guide Page', () => {
     cy.visit(urls.MaterialsGuide)
+    cy.title().should().not().be('Not found | Ernesta');
   })
 
   it('Privacy Policy Page', () => {
     cy.visit(urls.PrivacyPolicy)
+    cy.title().should().not().be('Not found | Ernesta');
   })
 
   it('Size Guide Page', () => {
     cy.visit(urls.SizeGuide)
+    cy.title().should().not().be('Not found | Ernesta');
   })
 
   it('Support Page', () => {
     cy.visit(urls.Support)
+    cy.title().should().not().be('Not found | Ernesta');
   })
 
   it('Terms of Use Page', () => {
     cy.visit(urls.TermsOfUse)
+    cy.title().should().not().be('Not found | Ernesta');
   })
 
   it('Trade Program Page', () => {
     cy.visit(urls.TradeProgram)
+    cy.title().should().not().be('Not found | Ernesta');
   })
 })
