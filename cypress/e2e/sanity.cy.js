140247dccd85f19d620fa482d9ecc7795c143ef0
luigimatchps <luigi.c@matchps.com>
2023-05-31 18:05:18 -0300

    updating tests

--- cypress/e2e/sanity.cy.js
+++ cypress/e2e/sanity.cy.js
@@ -1,62 +1,108 @@
-import urls from '../fixtures/urls.json' 
-describe("Navigate to Ernesta's Pages", () => { 
+import urls from '../fixtures/urls.json';
+import FooterPage from '../pages/FooterPage';
+
+describe("Navigate to Ernesta's Pages URLS", () => { 
   it('Homepage', () => {
     cy.visit(urls.Base)
-    cy.title().should().not().be('Not found | Ernesta');
+    cy.title().should('eq', 'Custom Size Area Rugs | Ernesta');
   })
 
   it('Product Listing Page', () => {
     cy.visit(urls.PLP)
-    cy.title().should().not().be('Not found | Ernesta');
+    cy.title().should('eq', 'Shop All Custom Cut Rugs | Ernesta');
   })
 
   it('About Us Page', () => {
     cy.visit(urls.AboutUs)
-    cy.title().should().not().be('Not found | Ernesta');
+    cy.title().should('eq', 'Why Ernesta | Ernesta');
   })
 
   it('Beta Launch Page', () => {
     cy.visit(urls.BetaLaunch)
-    cy.title().should().not().be('Not found | Ernesta');
+    cy.title().should('eq', 'Welcome to Beta | Ernesta');
   })
 
   it('Care Guide Page', () => {
     cy.visit(urls.CareGuide)
-    cy.title().should().not().be('Not found | Ernesta');
+    cy.title().should('eq', 'Rug Care Guide | Ernesta');
   })
 
   it('Limited Warranty Page', () => {
     cy.visit(urls.LimitedWarranty)
-    cy.title().should().not().be('Not found | Ernesta');
+    cy.title().should('eq', 'Limited Warranty | Ernesta');
   })
 
   it('Materials Guide Page', () => {
     cy.visit(urls.MaterialsGuide)
-    cy.title().should().not().be('Not found | Ernesta');
+    cy.title().should('eq', 'Rug Materials Guide: How to Choose the Right Rug | Ernesta');
   })
 
   it('Privacy Policy Page', () => {
     cy.visit(urls.PrivacyPolicy)
-    cy.title().should().not().be('Not found | Ernesta');
+    cy.title().should('eq', 'Privacy Policy | Ernesta');
   })
 
   it('Size Guide Page', () => {
     cy.visit(urls.SizeGuide)
-    cy.title().should().not().be('Not found | Ernesta');
+    cy.title().should('eq', 'Custom Rug Sizing Guide | Ernesta');
   })
 
   it('Support Page', () => {
     cy.visit(urls.Support)
-    cy.title().should().not().be('Not found | Ernesta');
+    cy.title().should('eq', 'Support | Ernesta');
   })
 
   it('Terms of Use Page', () => {
     cy.visit(urls.TermsOfUse)
-    cy.title().should().not().be('Not found | Ernesta');
+    cy.title().should('eq', 'Terms of Use | Ernesta');
   })
 
   it('Trade Program Page', () => {
     cy.visit(urls.TradeProgram)
-    cy.title().should().not().be('Not found | Ernesta');
+    cy.title().should('eq', 'Trade Program | Ernesta');
+  })
+
+})
+describe("Navigate to Ernesta's Pages through clicking the links in the footer", () => {
+  it('Beta Launch', () => {
+    cy.visit(urls.Base)
+    FooterPage.clickBetaLaunchLink()
+    cy.title().should('eq', 'Welcome to Beta | Ernesta');
+  })
+
+  it('Get in touch', () => {
+    cy.visit(urls.Base)
+    FooterPage.clickGetInTouchLink()
+    cy.title().should('eq', 'Get in touch | Ernesta');
+  })
+
+  it('Trade Program', () => {
+    cy.visit(urls.Base)
+    FooterPage.clickTradeProgramLink()
+    cy.title().should('eq', 'Trade Program | Ernesta');
+  })
+
+  it('Care Guide', () => {
+    cy.visit(urls.Base)
+    FooterPage.clickCareGuideLink()
+    cy.title().should('eq', 'Rug Care Guide | Ernesta');
+  })
+
+  it('Materials Guide', () => {
+    cy.visit(urls.Base)
+    FooterPage.clickMaterialsGuideLink()
+    cy.title().should('eq', 'Rug Materials Guide: How to Choose the Right Rug | Ernesta');
+  })
+
+  it('Size Guide', () => {
+    cy.visit(urls.Base)
+    FooterPage.clickSizeGuideLink()
+    cy.title().should('eq', 'Size Guide | Ernesta');
+  })
+
+  it('Why Ernesta', () => {
+    cy.visit(urls.Base)
+    FooterPage.clickWhyErnestaLink()
+    cy.title().should('eq', 'Why Ernesta | Ernesta');
   })
 })
--- cypress/pages/FooterPage.js
+++ cypress/pages/FooterPage.js
@@ -2,11 +2,39 @@ class FooterPage {
     elements = {
         whyErnestaLink: () => cy.get('span').contains('Why Ernesta').parent(),
         sizeGuideLink: () => cy.get('span').contains('Size guide').parent(),
-        whyErnestaLink: () => cy.get('span').contains('Materials guide').parent(),
-        sizeGuideLink: () => cy.get('span').contains('Care guide').parent(),        
-        whyErnestaLink: () => cy.get('span').contains('Trade program').parent(),
-        sizeGuideLink: () => cy.get('span').contains('Get in touch').parent(),     
-        whyErnestaLink: () => cy.get('span').contains('Beta launch').parent()
-    };    
+        materialsGuideLink: () => cy.get('span').contains('Materials guide').parent(),
+        careGuideLink: () => cy.get('span').contains('Care guide').parent(),        
+        tradeProgramLink: () => cy.get('span').contains('Trade program').parent(),
+        getInTouchLink: () => cy.get('span').contains('Get in touch').parent(),     
+        betaLaunchLink: () => cy.get('span').contains('Beta launch').parent()
+    };
+    
+    clickBetaLaunchLink() {
+        this.elements.betaLaunchLink.click();
+    }
+
+    clickGetInTouchLink() {
+        this.elements.getInTouchLink.click();
+    }
+
+    clickTradeProgramLink() {
+        this.elements.tradeProgramLink.click();
+    }
+
+    clickCareGuideLink() {
+        this.elements.careGuideLink.click();
+    }
+
+    clickMaterialsGuideLink() {
+        this.elements.materialsGuideLink.click();
+    }
+
+    clickSizeGuideLink() {
+        this.elements.sizeGuideLink.click();
+    }
+
+    clickWhyErnestaLink() {
+        this.elements.whyErnestaLink.click();
+    }
 }
-module.export = new FooterPage();
+module.exports = new FooterPage();
