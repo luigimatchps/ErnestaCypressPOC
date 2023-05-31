473fb2f68dcd2b95fba0f97c140534bdc6b9149e
luigimatchps <luigi.c@matchps.com>
2023-05-31 16:49:03 -0300

    Added elements to footer page

--- cypress/pages/FooterPage.js
+++ cypress/pages/FooterPage.js
@@ -1,6 +1,12 @@
 class FooterPage {
     elements = {
-
-    };
+        whyErnestaLink: () => cy.get('span').contains('Why Ernesta').parent(),
+        sizeGuideLink: () => cy.get('span').contains('Size guide').parent(),
+        whyErnestaLink: () => cy.get('span').contains('Materials guide').parent(),
+        sizeGuideLink: () => cy.get('span').contains('Care guide').parent(),        
+        whyErnestaLink: () => cy.get('span').contains('Trade program').parent(),
+        sizeGuideLink: () => cy.get('span').contains('Get in touch').parent(),     
+        whyErnestaLink: () => cy.get('span').contains('Beta launch').parent()
+    };    
 }
 module.export = new FooterPage();
